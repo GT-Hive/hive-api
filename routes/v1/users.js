a331c3579a1b5838a47111b6e7c927258a832325
Joshua Wang <joshuawang3110@gmail.com>
2019-01-21 20:10:02 -0500

    Refactor user routes

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -1,16 +1,11 @@
 const db = require('../../models');
+const userParams = ['id', 'email', 'first_name', 'last_name', 'intro', 'profile_img'];
 
-exports.getUsers = (req, res) => {  
-  db.User.findAll().then(function (users) {
-    res.json({ users: users });
-  });
+exports.getUsers = (req, res) => {
+  db.User.findAll({ attributes: userParams }).then((users) => res.json({ users }));
 };
 
 exports.getUser = (req, res) => {
   const id = req.params.id;
-  db.User.findById(id).then(user => {
-    res.json({ user });
-  });
+  db.User.findOne({ where: { id }, attributes: userParams }).then(user => res.json({ user }));
 };
-
-exports.userParams = ['id', 'email', 'first_name', 'last_name', 'intro', 'profile_img'];
